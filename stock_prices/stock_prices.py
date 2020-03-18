95dd2035ab495519099dd47ff9f208fd0973d3ac
Joshua Edgerton <joshuaxedgerton@gmail.com>
2020-03-18 17:08:10 -0500

    stock_prices.py buys high and sells low

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,29 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    #Declare "smallest" equal to the first element in the given array
+    smallest = prices[0]
+    #For each index in the array
+    for i in range(len(prices)):
+        #if that index is less than the previously set smallest value AND that index does not equal the same value of the last element in prices
+        if prices[i] < smallest and prices[i] != prices[len(prices) - 1]:
+            #Then "smallest" equals that index
+            smallest = prices[i]
+    
+    #"Smallest_value" equals the smallest index value in prices
+    smallest_value_index = prices.index(smallest)
+    #"shortened_prices" equals the element within prices thats just 1 higher in the array
+    shortened_prices = prices[smallest_value_index + 1 :]
+    largest = shortened_prices[0]
+
+    #For each index in range of the whole "shortened_prices" array
+    for i in range(len(shortened_prices)):
+        #if that index is greater than "largest"
+        if shortened_prices[i] > largest:
+            #"largest" then equals that index
+            largest = shortened_prices[i]
+    #Return the largest buy minus the smallest sell
+    return largest - smallest
 
 
 if __name__ == '__main__':
